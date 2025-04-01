6b321bc66ca160bf5b51a192fa5a2a188db1b87c
ahenaon00 <ahenao.2005@gmail.com>
2025-03-31 19:33:35 -0500

    Proyecto Spring Boot inicial

---
+++ src/main/java/trio/distribuidos/proyectodistribuidos/ProyectodistribuidosApplication.java
@@ -0,0 +1,13 @@
+package trio.distribuidos.proyectodistribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectodistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectodistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trio/distribuidos/proyectodistribuidos/ProyectodistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package trio.distribuidos.proyectodistribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectodistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
